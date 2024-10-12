bba97391831e631310e58c82574ae865197dbafa
Dawit-y <dawityimer52@gmail.com>
2024-10-12 15:57:43 +0300

    refactor: change fonts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <p className="uppercase tracking-loose w-full">
               What business are you?
             </p>
-            <h1 className="my-4 text-5xl font-bold leading-tight">
+            <h1 className="my-4 text-5xl font-bold leading-tight font-poppins">
               Main Hero Message to sell yourself!
             </h1>
             <p className="leading-normal text-2xl mb-8">
@@ -273,7 +273,7 @@ const Home = () => {
         <div className="w-full mb-4">
           <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
-        <h3 className="my-4 text-3xl leading-tight">
+        <h3 className="my-4 text-3xl leading-tight font-poppins">
           Main Hero Message to sell yourself!
         </h3>
         <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
